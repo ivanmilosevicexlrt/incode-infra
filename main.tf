ef14eccb6bb562e2b14669370d6b7cab76bb51d6
Ivan Milosevic <gornjagorevnica@gmail.com>
2026-02-22 17:49:58 +0100

    debugging

--- main.tf
+++ main.tf
@@ -5,25 +5,27 @@ module "vpc" {
   vpc_cidr          = "10.1.0.0/16"
   az_count          = 3
   enable_monitoring = true
-  enable_nat        = true
+  enable_nat = true
 }
 
-module "eks" {
-  source             = "./modules/eks"
-  cluster_name       = "prod-eks"
-  subnet_ids         = module.vpc.app_subnets
-  node_desired_size  = 3
-  node_min_size      = 3
-  node_max_size      = 6
-  node_instance_type = "t3.micro"
-}
+# module "eks" {
+#   source             = "./modules/eks"
+#   name       = "prod-eks"
+#   subnet_ids         = module.vpc.app_subnets
+#   node_desired_size  = 3
+#   node_min_size      = 3
+#   node_max_size      = 6
+#   node_instance_type = "t3.micro"
+# }
 
-module "database" {
-  source             = "./modules/database"
-  db_engine          = "rds" # or "aurora"
-  environment        = "prod"
-  db_username        = data.aws_ssm_parameter.dbuser.value
-  db_password        = data.aws_ssm_parameter.dbpassword.value
-  subnet_group_name  = module.vpc.db_subnet_group
-  security_group_ids = [module.eks.sg_id]
-}
+# module "database" {
+#   source             = "./modules/database"
+#   db_engine          = "rds" # or "aurora"
+#   environment        = "prod"
+#   db_username        = data.aws_ssm_parameter.dbuser.value
+#   db_password        = data.aws_ssm_parameter.dbpassword.value
+#   subnet_group_name  = module.vpc.db_subnet_group
+#   security_group_ids = [module.eks.sg_id]
+
+#   depends_on = [ module.eks ] #debug
+# }
