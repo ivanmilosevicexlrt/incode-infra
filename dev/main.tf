41d56ba284ed3da9fd4fe66d5c2164d0f7fc0f7d
Ivan Milosevic <gornjagorevnica@gmail.com>
2026-02-23 23:21:30 +0100

    sync with prod

--- dev/main.tf
+++ dev/main.tf
@@ -8,24 +8,24 @@ module "vpc" {
   enable_nat        = true
 }
 
-module "eks" {
-  source             = "../modules/eks"
-  name               = "dev-eks"
-  subnet_ids         = module.vpc.app_subnets
-  node_desired_size  = 1
-  node_min_size      = 1
-  node_max_size      = 2
-  node_instance_type = "t3.medium"
-}
-
-# module "database" {
-#   source             = "../modules/database"
-#   db_engine          = "rds"
-#   environment        = "dev"
-#   db_username        = data.aws_ssm_parameter.dbuser.value
-#   db_password        = data.aws_ssm_parameter.dbpassword.value
-#   subnet_group_name  = module.vpc.db_subnet_group
-#   security_group_ids = [module.eks.sg_id]
-#
-#   depends_on = [module.eks]
+# module "eks" {
+#   source             = "../modules/eks"
+#   name               = "dev-eks"
+#   subnet_ids         = module.vpc.app_subnets
+#   node_desired_size  = 1
+#   node_min_size      = 1
+#   node_max_size      = 2
+#   node_instance_type = "t3.medium"
 # }
+
+module "database" {
+  source             = "../modules/database"
+  db_engine          = "rds"
+  environment        = "dev"
+  db_username        = data.aws_ssm_parameter.dbuser.value
+  db_password        = data.aws_ssm_parameter.dbpassword.value
+  subnet_group_name  = module.vpc.db_subnet_group
+  
+  #security_group_ids = [module.eks.sg_id]
+  #depends_on = [ module.eks ] #debug
+}
--- dev/output.tf
+++ dev/output.tf
@@ -1,16 +1,7 @@
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+output "db_endpoint" {
+  value = module.database.db_endpoint
 }
 
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+output "db_reader_endpoint" {
+  value = module.database.db_reader_endpoint
 }
-
-# Uncomment when database module is enabled
-# output "db_endpoint" {
-#   value = module.database.db_endpoint
-# }
-#
-# output "db_reader_endpoint" {
-#   value = module.database.db_reader_endpoint
-# }
